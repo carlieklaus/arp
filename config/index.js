c2ec5b488bfe5199b45e66399248ded0df663328
Carlie Klaus <carlie.klaus@authorreputationpress.com>
2022-01-07 01:52:40 +0800

    changed books per page

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337/";
 
-export const PER_PAGE = 4;
+export const PER_PAGE = 8;
 
 export const ROYALTY_PER_PAGE = 5;
