30b5838124dc9a0daa5f2692bea5dff0637a5142
Carlie Klaus <carlie.klaus@authorreputationpress.com>
2022-01-15 03:09:43 +0800

    added sample og meta tags

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,34 @@ import BigdataFunFacts from "@/components/BigdataAnalytics/BigdataFunFacts";
 import Footer from "@/components/_App/Footer";
 import Projects from "@/components/MachineLearning/Projects";
 import Features from "@/components/IOT/Features";
+import Head from "next/head";
 
 const BigdataAnalytics = () => {
   return (
     <>
+      <Head>
+        <meta
+          name="description"
+          content="Author Reputation Press is where you can find the most reliable team of author consultants with a combined experience of more than two decades in the industry."
+        />
+        <meta property="og:type" content="article" />
+
+        <meta property="og:title" content="Author Reputation Press LLC" />
+
+        <meta
+          property="og:description"
+          content="Author Reputation Press is where you can find the most reliable team of author consultants with a combined experience of more than two decades in the industry."
+        />
+
+        <meta property="og:image" content="/images/logo-book.png" />
+
+        <meta
+          property="og:url"
+          content="https://usa.authorreputationpress.com/"
+        />
+
+        <meta property="og:site_name" content="Author Reputation Press LLC" />
+      </Head>
       <NavbarStyleFour textLogo="white" />
       <MainBanner />
       <Projects />
