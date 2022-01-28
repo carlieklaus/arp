c29e84c9fe53cdd5e53c483910de0f78956dccf6
Carlie Klaus <carlie.klaus@authorreputationpress.com>
2022-01-29 00:26:01 +0800

    updated sitemap generator

--- next-sitemap.js
+++ next-sitemap.js
@@ -2,8 +2,21 @@ module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_URL || "https://authorreputationpress.com/",
   generateRobotsTxt: true, // (optional)
   sitemapSize: 7000,
-  exclude: ["/upload-authors", "/upload-royalties", "/serversitemap.xml"],
+  exclude: [
+    "/upload-authors",
+    "/upload-royalties",
+    "/serversitemap.xml",
+    "/login",
+    "/sitemap.xml",
+    "/sitemap-0.xml",
+  ],
   robotsTxtOptions: {
-    additionalSitemaps: ["/serversitemap.xml"],
+    additionalSitemaps: ["https://authorreputationpress.com/serversitemap.xml"],
+    policies: [
+      {
+        userAgent: "*",
+        disallow: ["/login", "/upload-royalties", "/upload-authors"],
+      },
+    ],
   },
 };
