547f29a6f5af49fc0c98419f5c2f250c695335b4
Carlie Klaus <carlie.klaus@authorreputationpress.com>
2022-02-18 06:14:29 +0800

    removed changefreq and priority

--- next-sitemap.js
+++ next-sitemap.js
@@ -27,4 +27,12 @@ module.exports = {
       },
     ],
   },
+  transform: async (config, path) => {
+    // Use default transformation for all other cases
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
 };
