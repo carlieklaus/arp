44a38e146f03881eef0013f91b62a324b28e173e
Carlie Klaus <carlie.klaus@authorreputationpress.com>
2022-03-25 00:38:10 +0800

    changes for hollywood book to screen page

--- pages/services/powerhouse/hollywood-book-to-screen.js
+++ pages/services/powerhouse/hollywood-book-to-screen.js
@@ -61,27 +61,26 @@ const HollywoodBookToScreen = () => {
                   </div>
                   <div className="bar"></div>
                 </div>
-                <h4>
+                <h4 style={{ textAlign: "center" }}>
                   Hollywood Book-to-Screen Service For Your Book's Film
                   Adaptation
                 </h4>
-
-                <h5 style={{ marginTop: "2rem" }}>
-                  Hollywood Screen Writers For Hire
-                </h5>
                 <p style={{ marginTop: "1rem" }}>
-                  See your book transform into motion picture through our
-                  Book-to-Screen service. A film adaptation will be developed
-                  based on your story after a thorough scrutiny and analysis of
-                  it. We will evaluate your book’s cover, title, character,
-                  plot, and all its creative components.
+                  With the Book-to-Screen service, you can see your book
+                  transformed into a motion picture. After a thorough
+                  examination and study of the story, a cinematic version will
+                  be created. The cover, title, character, plot, and all other
+                  creative aspects of your book will be considered.
                 </p>
+
                 <div style={{ marginTop: "2rem" }}>
-                  <h5>Movie Script Coverage</h5>
+                  <h5 style={{ textDecoration: "underline" }}>
+                    Hollywood Movie Script Coverage{" "}
+                  </h5>
                   <p style={{ marginTop: "1rem" }}>
                     Let us create a written outline of your book in a database
                     through our Movie Script Coverage and be discovered by major
-                    studios in search for stories to adapt into motion pictures.
+                    studios in search of stories to adapt into motion pictures.
                     To be considered for a movie adaptation, your book needs to
                     have these basic elements: a cover sheet, synopsis, and
                     comments. We will endorse your manuscript to movie
@@ -93,83 +92,113 @@ const HollywoodBookToScreen = () => {
                 </div>
 
                 <div style={{ marginTop: "2rem" }}>
-                  <h5>Our Movie Script Coverage Features</h5>
+                  <h5 style={{ textDecoration: "underline" }}>
+                    Hollywood Movie Treatment{" "}
+                  </h5>
                   <p style={{ marginTop: "1rem" }}>
-                    A carefully written summary of your work showcasing its
-                    characters and plot. Comments about the strengths and
-                    weaknesses of your book as a factor for consideration of
-                    your work’s potential movie or TV adaptation. US Copyright
-                    Office application to grant you full ownership of your
-                    coverage, including those conducted during your pitch
-                    appointments.
+                    Movie Treatment service is one of the first steps in writing
+                    a screenplay, and it aims to provide a summary of your
+                    work's screenplay idea and give you the Hollywood style of
+                    fame that only a few authors have experienced. This package
+                    includes a well-written, detailed story produced for the
+                    screen (film, television, or digital) of your book's
+                    screenplay idea, presenting its theme, plot, and characters
+                    in a way that will captivate readers. Your book will then be
+                    registered in an online database, making it more visible to
+                    movie makers looking for latest content. Suggestions and
+                    expectations are laid down to the agencies, studios, and
+                    producers by stating exactly how to approach the adaptation
+                    of your book. You can effectively demonstrate to them that
+                    your book has what it takes to succeed on the big screen by
+                    providing them with a visual sketch of how to adapt it for
+                    the big screen.
                   </p>
-                </div>
-
-                <div style={{ marginTop: "2rem" }}>
-                  <h5>Movie Treatment</h5>
                   <p style={{ marginTop: "1rem" }}>
-                    Our Movie Treatment service aims to produce a synopsis of
-                    your work’s screenplay idea and let you experience a
-                    Hollywood-style of fame exclusive only to a few authors.
-                    This package includes the creation of a well-constructed
-                    7-10 page summary of your book’s screenplay idea,
-                    highlighting its thought, plot, and characters in a way that
-                    will take readers by storm. Your book will then be
-                    registered in an online database to increase its visibility
-                    to movie producers in search for interesting stories.
+                    This package includes the following services:{" "}
                   </p>
-                  <h6>This Package Includes The Following Services:</h6>
                   <ul>
                     <li>
-                      A 7-10 page treatment detailing how to convert your
-                      manuscript into a movie or TV series
+                      <p>
+                        Professional screenwriters who have experience adapting
+                        books to film and television will write a 7–10-page
+                        treatment detailing how to convert your manuscript into
+                        a movie or TV series
+                      </p>
                     </li>
                     <li>
-                      Directory registration of your movie treatment to gain
-                      traction among movie reviewers and film producers
+                      <p>
+                        Directory registration of your movie treatment to gain
+                        traction among movie reviewers and film producers
+                      </p>
                     </li>
+
                     <li>
-                      US Copyright Office application to help you retain full
-                      ownership of your treatment, which you can use during your
-                      pitch appointments on your screenplay applications
+                      <p>
+                        US Copyright Office application to help you retain full
+                        ownership of your treatment, which you can use during
+                        your pitch appointments on your screenplay applications
+                      </p>
                     </li>
                   </ul>
                 </div>
 
                 <div style={{ marginTop: "2rem" }}>
-                  <h5>Movie Screenplay</h5>
+                  <h5 style={{ textDecoration: "underline" }}>
+                    Hollywood Movie Screenplay
+                  </h5>
                   <p style={{ marginTop: "1rem" }}>
-                    Your story’s path to Hollywood fame rests entirely on your
-                    hands and we want to help you chart that course with our
+                    Your story’s path to Hollywood fame rests entirely in your
+                    hands, and we want to help you chart that course with our
                     Movie Screenplay service. We are connected to a network of
                     film producers who can help turn your book into a
                     blockbuster movie through creative concept video production,
-                    post-production, multimedia, and advertising services. The
-                    screenplay will focus on your story’s pivotal scenes, major
-                    characters, and the dialogue that gives life to the story.
+                    post-production, multimedia, and advertising services. A
+                    screenplay is a professionally written script that
+                    television and movie producers can use to see whether they
+                    wish to make a film adaptation of your book. You will show
+                    Hollywood that you are serious about breaking into the
+                    entertainment industry if you have a properly adapted
+                    screenplay. The screenplay will focus on your story’s
+                    pivotal scenes, major characters, and the dialogue that
+                    gives life to the story.
                   </p>
-                  <h6>Our Movie Screenplay Package Includes:</h6>
+                  <p style={{ marginTop: "1rem" }}>
+                    The Movie Screenplay Package includes:
+                  </p>
                   <ul>
                     <li>
-                      A 7-10 page treatment detailing the strategy to transform
-                      your manuscript into a movie or TV adaptation
+                      <p>
+                        A carefully written screenplay by one of the first-rate
+                        screenwriters based on your approved movie treatment
+                      </p>
                     </li>
                     <li>
-                      A carefully written screenplay by one of our first-rate
-                      screenwriters based on your approved movie treatment
+                      <p>
+                        Character dialogue and detailed story events will be
+                        included in the screenplay, which will be created using
+                        industry-standard formatting and tools.
+                      </p>
+                    </li>
+
+                    <li>
+                      <p>
+                        Submission of your draft screenplay to a producer for
+                        review in preparation for a film adaptation Directory
+                      </p>
+                    </li>
+
+                    <li>
+                      <p>
+                        Registration of your screenplay to gain attention from
+                        co-writers, movie reviewers, and producers
+                      </p>
                     </li>
                     <li>
-                      Submission of your draft screenplay to a producer for
-                      review in preparation for a film adaptation Directory
-                    </li>
-                    <li>
-                      Registration of your screenplay to gain attention from
-                      co-writers, movie reviewers, and producers
-                    </li>
-                    <li>
-                      US Copyright Office application to help you retain full
-                      ownership of your screenplay, which you can use to produce
-                      your own indie film
+                      <p>
+                        US Copyright Office application to help you retain full
+                        ownership of your screenplay, which you can use to
+                        produce your own indie film
+                      </p>
                     </li>
                   </ul>
                 </div>
