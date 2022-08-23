4f0ecb46a794dd756720a021b6ef53079d9ce159
Carlie Klaus <carlie.klaus@authorreputationpress.com>
2022-08-24 03:00:48 +0800

    separated phone and disclaimer for checkout

--- pages/_document.js
+++ pages/_document.js
@@ -48,7 +48,9 @@ class MyDocument extends Document {
                   <snipcart-input name="phone"></snipcart-input>
                 </div>
               </fieldset>
+            </billing>
 
+            <billing section="bottom">
               <fieldset class="snipcart-form__set">
                 <div class="snipcart-form__field">
                   <div class="snipcart-form__field-checkbox">
